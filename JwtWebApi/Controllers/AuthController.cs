947bffb26c5fcf7c4facab8f50e13bc57ad90456
Abner pires urbano <abner.up@hotmail.com>
2022-12-12 18:45:39 -0300

    Add Role Based Autorization

--- JwtWebApi/Controllers/AuthController.cs
+++ JwtWebApi/Controllers/AuthController.cs
@@ -55,7 +55,8 @@ namespace JwtWebApi.Controllers
 
             List<Claim> claimsList = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.UserName)
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
